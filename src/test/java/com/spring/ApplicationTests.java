b798d45a3502979f203e6cb6cb25c3d277bbd4bd
calperfella <pdcalderon88@ucatolica.edu.co>
2024-11-22 13:47:50 -0500

    Correccion test aplicattion

--- src/test/java/com/spring/ApplicationTests.java
+++ src/test/java/com/spring/ApplicationTests.java
@@ -2,12 +2,14 @@ package com.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Activar el perfil "test" definido en application-test.properties
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Verifica que el contexto de Spring se cargue correctamente
 	}
-
 }
